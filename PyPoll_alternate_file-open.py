a18fabe2426bbc83afaf938a036b16b2ae7adfe9
josephthompson532 <66881241+josephthompson532@users.noreply.github.com>
2020-08-02 20:27:03 -0700

    I am uploading the final versions of my work

--- PyPoll_alternate_file-open.py
+++
@@ -1,9 +0,0 @@
-file_to_load = 'Resources/election_results.csv'
-
-#Open the elction results and read the file
-work_with_var = open(file_to_load, 'r')
-
-#Do analysis and perform code
-
-# Close the file.
-work_with_var.close()
